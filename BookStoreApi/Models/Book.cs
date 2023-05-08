685fe86466d662692d803d8933a9766236cdfe03
jeevallucas <jnanamaitriya@gmail.com>
2023-05-08 13:37:25 +0700

    Pertemuan 9 DRWA

--- BookStoreApi/Models/Book.cs
+++ BookStoreApi/Models/Book.cs
@@ -12,19 +12,12 @@ public class Book
     public string? Id { get; set; }
 
     [BsonElement("Name")]
-    [JsonPropertyName("Name")]
-    [Required]
-    [StringLength(100, MinimumLength = 2)]
     public string BookName { get; set; } = null!;
-
+    
     [Required]
-    [Range(0, 999)]
-    public decimal Price { get; set; }
+    public decimal? Price { get; set; } = null!;
 
-    [Required]
-    [StringLength(100, MinimumLength = 2)]
     public string Category { get; set; } = null!;
-
-    [Required]
+    
     public string Author { get; set; } = null!;
 }
